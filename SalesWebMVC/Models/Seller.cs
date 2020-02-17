0cdfb949645a40c745eb7d8824a55f690f2b72c8
Deborach <deborachrmv@gmail.com>
2020-02-17 12:54:48 -0300

    Seeding service

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -20,13 +20,14 @@ namespace SalesWebMVC.Models
         {
         }
 
-        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        public Seller(int id, string name, string email,  DateTime birthDate, double baseSalary,  Department department)
         {
             Id = id;
             Name = name;
             Email = email;
-            BaseSalary = baseSalary;
+            
             BirthDate = birthDate;
+            BaseSalary = baseSalary;
             Department = department;
         }
 
